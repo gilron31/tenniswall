437c1af4193c270739b9d0783eff5a87d24148fe
Gilro <gilron31@gmail.com>
2022-04-22 22:43:44 +0300

    done

---
+++ src/stft_peak_detection_demo.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+import sounddevice as sd
+
+BASE_SAMPLE_FREQUENCY_HZ = 44100
+DEFAULT_RECORDING_DURATION_S = 10
+STFT_N_SAMPLES_PER_SEG = 100
+STFT_OVERLAP_PER_SEG = 90
+STFT_HPF_BIN_THRES = 3
+
+def record_signal_with_sounddevice(fs, duration):
+	input(f"Will start recording for {duration} seconds")
+	s = sd.rec(fs*duration, fs, channels = 1)
+	sd.wait()
+	t = np.linspace(0, duration, fs*duration)
+	print("Done recording")
+	return t, [x[0] for x in s] #sd returns a vector of singletons, reformatting. 
+
+def detect_bangs(s, fs, duration):
+	assert(len(s) == fs*duration)
+
+	f, t, zxx = signal.stft(s, fs, nperseg = STFT_N_SAMPLES_PER_SEG, noverlap = STFT_OVERLAP_PER_SEG)
+	hpf_power_signal = np.sum(abs(zxx[STFT_HPF_BIN_THRES:])**2, axis = 0)
+
+	## this is for testing, in the end the function should return timestamps
+	plt.figure()
+	plt.subplot(2,1,1)
+	plt.plot(t, hpf_power_signal)
+	plt.subplot(2,1,2)
+	plt.plot(np.linspace(0, duration, fs*duration), s)
+	plt.show()
+
+
+
+
+
+if __name__ == "__main__":
+	t, s = record_signal_with_sounddevice(BASE_SAMPLE_FREQUENCY_HZ, DEFAULT_RECORDING_DURATION_S)
+	detect_bangs(s, BASE_SAMPLE_FREQUENCY_HZ, DEFAULT_RECORDING_DURATION_S)
