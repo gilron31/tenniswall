2806b3295255f7d3aaf93c15b314e81bc24983c7
user <user@com>
2022-04-23 20:05:22 +0300

    Adding multiclient support

--- src/networking/server_app.py
+++ src/networking/server_app.py
@@ -1,12 +1,12 @@
 import socket 
 from enum import Enum
+import time
 import struct
 import sys
 import select
-from threading import Thread
-from collections import deque
+from threading import Thread, RLock
+import queue
 import sounddevice as sd
-
 
 SERVER_IDLE_MESSAGE = """\nServer is now idle.
 Options: 
@@ -19,6 +19,7 @@ NUMBER_FORMAT_BYTE_LENGTH = 4
 TIMESTAMP_PACKING_NUMBER_OF_BYTES = 2 * NUMBER_FORMAT_BYTE_LENGTH
 BASE_SAMPLE_FREQUENCY_HZ = 44100
 
+
 class ServerStates(Enum):
 	STARTUP = 1
 	ERROR = 2
@@ -26,6 +27,7 @@ class ServerStates(Enum):
 	WAITING_FOR_CLIENTS_RESPONSE = 4
 	SENDING_INSTRUCTIONS_TO_CLIENTS = 5
 	QUIT = 6
+
 
 class TennisServer(object):
 	SERVER_TIMEOUT_S = 10
@@ -41,7 +43,8 @@ class TennisServer(object):
 		self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		self.inputs = [self.socket]
 		self.outputs = []
-		self.parse_sound_ready_sockets = deque()
+		self.parse_sound_ready_sockets = queue.Queue()
+		self.printed_sockets = []
 
 	def start(self):
 		self.socket.bind((self.my_ip, self.my_port))
@@ -65,11 +68,14 @@ class TennisServer(object):
 			self.acquire_new_client()
 		else:
 			# non server socket will be readable when sound data was sent from client socket
-			if socket not in self.parse_sound_ready_sockets:
-				self.parse_sound_ready_sockets.append(socket)
+			if socket not in self.printed_sockets:
+				print("got response from", socket)
+				self.printed_sockets.append(socket)
+			if socket not in self.parse_sound_ready_sockets.queue:
+				self.parse_sound_ready_sockets.put(socket)
 
-	@staticmethod
-	def manage_sound_packet(socket):
+	def manage_sound_packet(self, socket):
+
 		"""
 		Will parse and extract sound packet from read ready socket
 		:param socket: socket to read sound from
@@ -86,7 +92,12 @@ class TennisServer(object):
 		timestamp = socket.recv(TIMESTAMP_PACKING_NUMBER_OF_BYTES)
 		timestamp = struct.unpack("Q", timestamp)[0]
 		data_length = packet_length - TIMESTAMP_PACKING_NUMBER_OF_BYTES
-		data = socket.recv(data_length)
+		read_bytes = 0
+		data = b''
+		while read_bytes < data_length:
+			data_chunk = socket.recv(1024)
+			read_bytes += len(data_chunk)
+			data += data_chunk
 		data = struct.unpack(f"{int(len(data) / 4)}f", data)
 
 		return timestamp, data
@@ -148,7 +159,7 @@ class TennisServer(object):
 			elif self.state == ServerStates.WAITING_FOR_CLIENTS_RESPONSE:
 				client_answers = self.wait_for_clients_response()
 				self.process_client_response_dict(client_answers)
-
+				self.state = ServerStates.IDLE
 			elif self.state == ServerStates.SENDING_INSTRUCTIONS_TO_CLIENTS:
 				self.send_instructions_to_clients()
 			elif self.state == ServerStates.QUIT:
@@ -163,8 +174,10 @@ class TennisServer(object):
 		:return: None
 		"""
 		# play the records:
-		for response in response_dict.values():
+		for i, response in enumerate(response_dict.values()):
+			print("Playing response", i)
 			sd.play(response[1], samplerate=BASE_SAMPLE_FREQUENCY_HZ)
+			time.sleep(4)
 
 
 	def get_client_by_socket(self, socket):
@@ -174,7 +187,7 @@ class TennisServer(object):
 		:return: Will return a tuple representing a connected client (item in self.clients list)
 		"""
 		for client in self.clients:
-			if client[0] == socket:
+			if client[0] is socket:
 				return client
 		raise ValueError(f"There is no connected client with a connection socket: {socket}")
 
@@ -184,10 +197,14 @@ class TennisServer(object):
 		:return: a dict mapping each registered client with its response
 		"""
 		client_responses = dict()
-		while len(client_responses) < len(self.clients):
-			if len(self.parse_sound_ready_sockets) > 0:
-				client_socket = self.parse_sound_ready_sockets.pop()
-				client_responses[self.get_client_by_socket(client_socket)] = self.manage_sound_packet(client_socket)
+		while self.parse_sound_ready_sockets.qsize() < len(self.clients):
+			import time
+			time.sleep(0.1)
+		for i in range(len(self.clients)):
+			client_socket = self.parse_sound_ready_sockets.get()
+			print("Processing queue element:", client_socket)
+			client = self.get_client_by_socket(client_socket)
+			client_responses[client] = self.manage_sound_packet(client_socket)
 		return client_responses
 
 	def idle(self):
